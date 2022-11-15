18bbbfde99736f7a1939a4083fab851ff80c24b7
wilgagne <william.gagne@mail.utoronto.ca>
2022-11-14 23:32:03 -0500

    multi-client test with 1 client

--- src/main.java
+++ src/main.java
@@ -1,62 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
 public class main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in));
-        System.out.println("WELCOME TO GUESS THE THREADS");
-        boolean play = true;
-        while (play) {
-            System.out.println("How many threads would you like to create?");
-            int n = Integer.parseInt(reader.readLine());
-            System.out.println("Guess the order of the threads\n" +
-                    "Enter your guess in the following format: 12435\n" +
-                    "__________________________________________________");
-            String guess = reader.readLine();
-            int[] arr = new int[n];
-            Thread[] threadArr = new Thread[n];
-            for (int i = 1; i < n + 1; i++) {
-                int finalI = i;
-                int[] finalArr = arr;
-                Thread sub = new Thread(() -> {
-                    // Thread safety, execute together, -lock
-                    synchronized (finalArr){
-                    finalArr[0] = finalArr[0] + 1;
-                    System.out.println(finalArr[0]);
-                }});
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 1) {
+            System.out.println("i is " + i);
+            for (int n = 1; n <= i; ++n){
+                System.out.println("Started a client");
+                clientWriter clientWriter = new clientWriter(n);
+                Thread newPrinter = new Thread(clientWriter);
+                newPrinter.start();
+            }
+            i = i * 2;
 
-                Thread sub2 = new Thread(() -> {
-                    // Thread safety, execute together, -lock
-                    synchronized (finalArr){
-                        finalArr[0] = finalArr[0] + 1;
-                        System.out.println(finalArr[0]);
-                    }});
-                threadArr[i - 1] = sub;
-                sub.start();
-            }
-            for (int i = 1; i < n + 1; i++) {
-                threadArr[i - 1].join();
-            }
-            System.out.println("The order is " + Arrays.toString(arr) + "\n");
-
-            int k = 0;
-            for (int i : arr) {
-                k = 10 * k + i;
-            }
-
-            if (guess.equals(String.valueOf(k))) {
-                System.out.println("You guessed it right :D\n");
-            } else {
-                System.out.println("Your guess was wrong :(");
-            }
-            System.out.println("Play again? answer Y or N");
-            String answer = reader.readLine();
-            if (!answer.equals("Y")) {
-                play = false;
+            try {
+                System.out.println("Thread asleep");
+                Thread.sleep(30000);
+                System.out.println("Thread awakened");
+            } catch (InterruptedException e){
+                System.out.println("Couldn't sleep");
             }
         }
+        System.out.println("DONE");
     }
 }
